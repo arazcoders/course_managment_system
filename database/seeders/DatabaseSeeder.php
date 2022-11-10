fa4578613a5ce52a4dad0accb6fae9ae6cf1066d
ArazCoders <arazcoders@gmail.com>
2022-11-10 11:45:50 +0330

    database seeder created!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +25,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('users')->insert([
+            'userName'=>'2740933762',
+            'name'=>'محمد',
+            'last_name'=>'جمشیدی',
+            'email'=>'arazcoders@gmail.com',
+            'password'=>Hash::make('MJamshidi73@')
+        ]);
+
+        Permission::create(['name' => 'manage_users']);
+
+        $admin= Role::create(['name' => 'admin']);
+        Role::create(['name' => 'teacher']);
+        Role::create(['name' => 'student']);
+
+        $admin->givePermissionTo('manage_users');
+
+        User::query()->findOrFail(1)->assignRole($admin);
+
     }
 }
